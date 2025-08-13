beec363bbe46610126ecb5abfdab3f68ead8b693
ivanpetrashchuk@gmail.com <petrashchukim@gmail.com>
2025-08-13 14:40:50 +0300

    inizilization project

---
+++ app/server/eslint.config.mjs
@@ -0,0 +1,61 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      // TypeScript-specific rules
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+
+      // Code style and best practices rules
+      // Enforce consistent indentation of 4 spaces.
+      indent: ['error', 4],
+      // Enforce spacing around infix operators (e.g., `a + b`).
+      'space-infix-ops': ['error', { int32Hint: false }],
+      // Enforce the use of single quotes for strings.
+      quotes: ['error', 'single'],
+      // Disallow the use of `var` in favor of `let` or `const`.
+      'no-var': 'error',
+      // Suggest using `const` for variables that are never reassigned.
+      'prefer-const': 'error',
+      // Enforce the use of strict equality operators (`===` and `!==`).
+      eqeqeq: ['error', 'always'],
+      // Disallow the use of `console` methods in production code.
+      // 'no-console': 'error',
+      // Disallow assignments in conditional expressions.
+      'no-cond-assign': ['error', 'always'],
+      // Disallow unnecessary semicolons.
+      'no-extra-semi': 'error',
+      // Disallow comments on the same line as code.
+      // 'no-inline-comments': 'error',
+      // Disallow redundant `else` statements after a `return` in an `if`.
+      'no-else-return': 'error',
+      // Enforce consistent use of curly braces for all control statements.
+      curly: ['error', 'all'],
+    },
+  },
+);
